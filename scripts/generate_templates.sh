4c0e3563f1d53a22ce2cc5256c950b579d94708d
leon <leonlenk@gmail.com>
2024-03-27 16:21:20 -0700

    Fixed file path bug

--- scripts/generate_templates.sh
+++ scripts/generate_templates.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 # tar everything including hidden files in the template
-tar czvf ../assets/skel.tar.gz -C /etc/skel/ .
+tar czvf ./assets/skel.tar.gz -C /etc/skel .
+
+sudo tar czvf ./assets/sys_scripts.tar.gz -C /scripts .
+
+sudo cp /arcblroth_acm_ai_logo.ans ./assets
+
+sudo cp /etc/bash.bashrc ./assets
--- scripts/load_templates.sh
+++ scripts/load_templates.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-tar xf ../assets/skel.tar.gz -C /etc/skel
-tar xf ../assets/skel.tar.gz -C $(eval echo ~$USER)
+sudo tar xf ./assets/skel.tar.gz -C /etc/skel
+tar xf ./assets/skel.tar.gz -C $(eval echo ~$USER)
+
+sudo mkdir /scripts/
+sudo tar xf ./assets/sys_scripts.tar.gz -C /scripts/
+
+sudo cp ./assets/arcblroth_acm_ai_logo.ans /
+
+sudo cp ./assets/bash.bashrc /etc/
