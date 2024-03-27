720c69d2940a90f5d6c90a7288e59561b7daef66
leon <leonlenk@gmail.com>
2024-03-27 12:57:46 -0700

    fixed anaconda installer

--- scripts/setup_anaconda.sh
+++ scripts/setup_anaconda.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-sudo mkdir /opt/anaconda3
-
 conda="anaconda.sh"
 
 wget https://repo.anaconda.com/archive/Anaconda3-2024.02-1-Linux-x86_64.sh -O $conda
 
 chmod +x $conda
 
-bash ./$conda -b -p /opt/anaconda3
+sudo bash ./$conda -b -p /opt/anaconda3
 
 chmod -R go-w /opt/anaconda3
 chmod -R go+rX /opt/anaconda3
