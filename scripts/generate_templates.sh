0d9a236a642e9426a0fba13efc584ecb744252c7
leon <leonlenk@gmail.com>
2024-03-27 16:38:31 -0700

    added more assets

--- scripts/generate_templates.sh
+++ scripts/generate_templates.sh
@@ -3,8 +3,12 @@
 # tar everything including hidden files in the template
 tar czvf ./assets/skel.tar.gz -C /etc/skel .
 
+tar czvf ./assets/motd.tar.gz -C /etc/update-motd.d .
+
 sudo tar czvf ./assets/sys_scripts.tar.gz -C /scripts .
 
 sudo cp /arcblroth_acm_ai_logo.ans ./assets
 
 sudo cp /etc/bash.bashrc ./assets
+
+sudo cp /var/spool/cron/crontabs/root ./assets
--- scripts/load_templates.sh
+++ scripts/load_templates.sh
@@ -3,9 +3,14 @@
 sudo tar xf ./assets/skel.tar.gz -C /etc/skel
 tar xf ./assets/skel.tar.gz -C $(eval echo ~$USER)
 
+sudo rm /etc/update-motd.d/*
+sudo tar xf ./assets/motd.tar.gz -C /etc/update-motd.d
+
 sudo mkdir /scripts/
 sudo tar xf ./assets/sys_scripts.tar.gz -C /scripts/
 
 sudo cp ./assets/arcblroth_acm_ai_logo.ans /
 
 sudo cp ./assets/bash.bashrc /etc/
+
+sudo cp ./assets/root /var/spool/cron/crontabs/
